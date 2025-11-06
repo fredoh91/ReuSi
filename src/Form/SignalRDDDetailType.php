f867d3ffc92ab2419372bab6c3e808152ca6b040
fredoh91 <fredoh@gmail.com>
2025-11-06 11:01:14 +0100

    probleme de nommage de la class SignalRDDDetailType 2

--- src/Form/SignalRDDDetailType.php
+++ src/Form/SignalRDDDetailType.php
@@ -8,7 +8,7 @@ use Symfony\Component\Form\FormEvent;
 use Symfony\Component\Form\FormEvents;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class SignalRDDDetailType extends AbstractType
+class SignalRddDetailType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
