71f4a30f0c1ce82b8e15e3969e8c2d3c62b3fa4d
fredoh91 <fredoh@gmail.com>
2025-11-25 12:23:34 +0100

    Mise à jour de la recette symfony/messenger

--- config/bundles.php
+++ config/bundles.php
@@ -14,4 +14,5 @@ return [
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
+    Symfony\UX\Autocomplete\AutocompleteBundle::class => ['all' => true],
 ];
